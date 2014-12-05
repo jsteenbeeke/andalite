ec94081b826bad26841edf0f116111e2895b1113
Jeroen Steenbeeke <jeroen.steenbeeke@topicus.nl>
2014-12-05 14:54:56 +0100

    License headers

--- src/test/java/com/jeroensteenbeeke/andalite/AnalyzerTest.java
+++ src/test/java/com/jeroensteenbeeke/andalite/AnalyzerTest.java
@@ -1,3 +1,17 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.jeroensteenbeeke.andalite;
 
 import static org.junit.Assert.*;
@@ -7,7 +21,6 @@ import java.util.List;
 
 import org.junit.Test;
 
-import com.jeroensteenbeeke.andalite.TypedActionResult;
 import com.jeroensteenbeeke.andalite.analyzer.*;
 import com.jeroensteenbeeke.andalite.analyzer.annotation.AnnotationValue;
 import com.jeroensteenbeeke.andalite.analyzer.annotation.ArrayValue;
@@ -18,7 +31,7 @@ public class AnalyzerTest {
 	public void testRewriter() {
 		ClassAnalyzer classAnalyzer = new ClassAnalyzer(
 				new File(
-						"src/test/java/com/jeroensteenbeeke/hyperion/morph/RewriterTest.java"));
+						"src/test/java/com/jeroensteenbeeke/andalite/RewriterTest.java"));
 		TypedActionResult<AnalyzedSourceFile> result = classAnalyzer.analyze();
 		assertTrue(result.isOk());
 
@@ -28,7 +41,7 @@ public class AnalyzerTest {
 		assertEquals(1, classes.size());
 		AnalyzedClass analyzedClass = classes.get(0);
 
-		assertEquals("com.jeroensteenbeeke.hyperion.morph",
+		assertEquals("com.jeroensteenbeeke.andalite",
 				analyzedClass.getPackageName());
 		assertEquals("RewriterTest", analyzedClass.getClassName());
 		assertTrue(analyzedClass.hasAnnotation("Ignore"));
@@ -47,21 +60,21 @@ public class AnalyzerTest {
 	public void testAnalyzer() {
 		ClassAnalyzer classAnalyzer = new ClassAnalyzer(
 				new File(
-						"src/test/java/com/jeroensteenbeeke/hyperion/morph/AnalyzerTest.java"));
+						"src/test/java/com/jeroensteenbeeke/andalite/AnalyzerTest.java"));
 		TypedActionResult<AnalyzedSourceFile> result = classAnalyzer.analyze();
 		assertTrue(result.isOk());
 
 		AnalyzedSourceFile source = result.getObject();
 
 		assertTrue(source
-				.hasImport("com.jeroensteenbeeke.hyperion.morph.analyzer.ClassAnalyzer"));
+				.hasImport("com.jeroensteenbeeke.andalite.analyzer.ClassAnalyzer"));
 	}
 
 	@Test
 	public void testAnnotationScan() {
 		ClassAnalyzer classAnalyzer = new ClassAnalyzer(
 				new File(
-						"src/test/java/com/jeroensteenbeeke/hyperion/morph/analyzer/LolCat.java"));
+						"src/test/java/com/jeroensteenbeeke/andalite/analyzer/LolCat.java"));
 
 		TypedActionResult<AnalyzedSourceFile> result = classAnalyzer.analyze();
 		assertTrue(result.isOk());
@@ -127,7 +140,7 @@ public class AnalyzerTest {
 	public void outputTest() {
 		ClassAnalyzer classAnalyzer = new ClassAnalyzer(
 				new File(
-						"src/test/java/com/jeroensteenbeeke/hyperion/morph/analyzer/LolCat.java"));
+						"src/test/java/com/jeroensteenbeeke/andalite/analyzer/LolCat.java"));
 
 		TypedActionResult<AnalyzedSourceFile> result = classAnalyzer.analyze();
 		assertTrue(result.isOk());
