c75d23eba759e9dd823377b5ad5419ddd9239491
Jeroen Steenbeeke <j.steenbeeke@gmail.com>
2018-04-25 21:58:56 +0200

    Limited API of Answers

--- forge/src/main/java/com/jeroensteenbeeke/andalite/forge/ui/questions/Answers.java
+++ forge/src/main/java/com/jeroensteenbeeke/andalite/forge/ui/questions/Answers.java
@@ -1,8 +1,12 @@
 package com.jeroensteenbeeke.andalite.forge.ui.questions;
 
 import com.google.common.collect.ImmutableMap;
+import com.jeroensteenbeeke.andalite.java.analyzer.AnalyzedSourceFile;
+import com.jeroensteenbeeke.andalite.java.analyzer.ClassAnalyzer;
+import com.jeroensteenbeeke.hyperion.util.TypedResult;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.util.Map;
 
 public class Answers {
@@ -26,12 +30,28 @@ public class Answers {
 		return answers.containsKey(key);
 	}
 
-	public boolean hasAnswer(@Nonnull Class<?> type, @Nonnull String key) {
+	public String getString(@Nonnull String key) {
+		return getAnswer(String.class, key);
+	}
+
+	public File getFile(@Nonnull String key) {
+		return getAnswer(File.class, key);
+	}
+
+	public boolean getBoolean(@Nonnull String key) {
+		return getAnswer(Boolean.class, key);
+	}
+
+	public TypedResult<AnalyzedSourceFile> getSource(@Nonnull String key) {
+		return new ClassAnalyzer(getFile(key)).analyze();
+	}
+
+	private boolean hasAnswer(@Nonnull Class<?> type, @Nonnull String key) {
 		return answers.containsKey(key) && type.isAssignableFrom(answers.get(key).getClass());
 	}
 
 	@SuppressWarnings("unchecked")
-	public <T> T getAnswer(@Nonnull Class<T> type, @Nonnull String key) {
+	private <T> T getAnswer(@Nonnull Class<T> type, @Nonnull String key) {
 		if (answers.containsKey(key)) {
 			Object object = answers.get(key);
 
