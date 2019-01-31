03126da311e4e83a91a163ad60974068f403acdc
Jeroen Steenbeeke <j.steenbeeke@gmail.com>
2019-01-31 22:40:11 +0100

    Also ignore whitespace in superclass check

--- java/src/main/java/com/jeroensteenbeeke/andalite/java/transformation/operations/impl/EnsureSuperClass.java
+++ java/src/main/java/com/jeroensteenbeeke/andalite/java/transformation/operations/impl/EnsureSuperClass.java
@@ -3,12 +3,12 @@
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,6 +23,8 @@ import com.jeroensteenbeeke.andalite.core.Transformation;
 import com.jeroensteenbeeke.andalite.core.exceptions.OperationException;
 import com.jeroensteenbeeke.andalite.java.analyzer.AnalyzedClass;
 import com.jeroensteenbeeke.andalite.java.transformation.operations.IClassOperation;
+
+import javax.annotation.Nonnull;
 
 public class EnsureSuperClass implements IClassOperation {
 	private final String superClass;
@@ -69,11 +71,20 @@ public class EnsureSuperClass implements IClassOperation {
 	}
 
 	@Override
-	public ActionResult verify(AnalyzedClass input) {
-		if (superClass.equals(input.getSuperClass())) {
+	public ActionResult verify(@Nonnull AnalyzedClass input) {
+		if (stripWhitespaces(superClass).equals(stripWhitespaces(input.getSuperClass()))) {
 			return ActionResult.ok();
 		}
 		return ActionResult.error("Invalid superclass: %s, expected %s",
 				input.getSuperClass(), superClass);
+	}
+
+
+	private String stripWhitespaces(String s) {
+		if (s == null) {
+			return null;
+		}
+		
+		return s.replaceAll("\\s", "");
 	}
 }
